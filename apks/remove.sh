2ada41c2af4141f902f6a71c3644f9e452192be9
kerta1n <36344851+kerta1n@users.noreply.github.com>
2022-08-05 17:27:02 -0400

    Update remove.sh

--- apks/remove.sh
+++ apks/remove.sh
@@ -1,10 +1,9 @@
-adb uninstall com.google.android.apps.mediashell
-adb uninstall jp.co.rarity.tvweb
-adb uninstall com.spotify.tv.android
-adb uninstall cm.aptoidetv.pt
-adb uninstall com.amazon.amazonvideo.livingroom
-adb uninstall com.teamsmart.videomanager.tv
-adb uninstall com.topjohnwu.magisk
-adb uninstall com.carriez.flutter_hbb
-adb uninstall com.netflix.ninja
-adb uninstall com.disney.disneyplus
+adb uninstall com.google.android.apps.mediashell # Chromecast built-in
+adb uninstall jp.co.rarity.tvweb # TVWeb browser
+adb uninstall com.spotify.tv.android # Spotify
+adb uninstall cm.aptoidetv.pt # Aptoide TV
+adb uninstall com.amazon.amazonvideo.livingroom # Prime Video
+adb uninstall com.teamsmart.videomanager.tv # Smarttube
+adb uninstall com.topjohnwu.magisk # Magisk
+adb uninstall com.netflix.ninja # Netflix
+adb uninstall com.disney.disneyplus # Disney+
